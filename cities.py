163b4e9f330c94bcf032c9296eeed809ce3b2542
Sebastian Staszczyk <sebastianstaszczyk.1999@gmail.com>
2023-12-08 20:14:54 +0100

    Add cities

---
+++ cities.py
@@ -0,0 +1,146 @@
+cities = [
+    {
+        "name": "Białystok",
+        "demand": 500,
+        "coordinates": {"x": 53.132398, "y": 23.1591679},
+    },
+    {
+        "name": "Bielsko-Biała",
+        "demand": 50,
+        "coordinates": {"x": 49.822118, "y": 19.0448936},
+    },
+    {
+        "name": "Chrzanów",
+        "demand": 400,
+        "coordinates": {"x": 50.1411926, "y": 19.4028502},
+    },
+    {
+        "name": "Gdańsk",
+        "demand": 200,
+        "coordinates": {"x": 54.3706858, "y": 18.6129821},
+    },
+    {
+        "name": "Gdynia",
+        "demand": 100,
+        "coordinates": {"x": 54.5164982, "y": 18.5402738},
+    },
+    {
+        "name": "Gliwice", 
+        "demand": 40, 
+        "coordinates": {"x": 50.294113, "y": 18.6657306}},
+    {
+        "name": "Gromnik",
+        "demand": 200,
+        "coordinates": {"x": 53.7361111, "y": 15.9897222},
+    },
+    {
+        "name": "Katowice",
+        "demand": 300,
+        "coordinates": {"x": 50.2598987, "y": 19.0215852},
+    },
+    {
+        "name": "Kielce", 
+        "demand": 30, 
+        "coordinates": {"x": 50.8719903, "y": 20.6310488}},
+    {
+        "name": "Krosno",
+        "demand": 60,
+        "coordinates": {"x": 49.68898435, "y": 21.7534788},
+    },
+    {
+        "name": "Krynica",
+        "demand": 50,
+        "coordinates": {"x": 53.0336093, "y": 23.5880673},
+    },
+    {
+        "name": "Lublin", 
+        "demand": 60, 
+        "coordinates": {"x": 51.250559, "y": 22.5701022}},
+    {
+        "name": "Łódź", 
+        "demand": 160, 
+        "coordinates": {"x": 51.7687323, "y": 19.4569911}},
+    {
+        "name": "Malbork",
+        "demand": 100,
+        "coordinates": {"x": 54.0358356, "y": 19.0368496},
+    },
+    {
+        "name": "Nowy Targ",
+        "demand": 120,
+        "coordinates": {"x": 49.4818229, "y": 20.030541},
+    },
+    {
+        "name": "Olsztyn",
+        "demand": 300,
+        "coordinates": {"x": 53.7766839, "y": 20.476507},
+    },
+    {
+        "name": "Poznań",
+        "demand": 100,
+        "coordinates": {"x": 52.4082663, "y": 16.9335199},
+    },
+    {
+        "name": "Puławy",
+        "demand": 200,
+        "coordinates": {"x": 51.4171713, "y": 21.9720582},
+    },
+    {
+        "name": "Radom", 
+        "demand": 100, 
+        "coordinates": {"x": 51.4022557, "y": 21.1541546}},
+    {
+        "name": "Rzeszów",
+        "demand": 60,
+        "coordinates": {"x": 50.0374531, "y": 22.0047174},
+    },
+    {
+        "name": "Sandomierz",
+        "demand": 200,
+        "coordinates": {"x": 50.6793066, "y": 21.7495055},
+    },
+    {
+        "name": "Szczecin",
+        "demand": 150,
+        "coordinates": {"x": 53.4301818, "y": 14.5509623},
+    },
+    {
+        "name": "Szczucin",
+        "demand": 60,
+        "coordinates": {"x": 50.3090256, "y": 21.075161},
+    },
+    {
+        "name": "Szklarska Poręba",
+        "demand": 50,
+        "coordinates": {"x": 50.8268011, "y": 15.5221261},
+    },
+    {
+        "name": "Tarnów", 
+        "demand": 70, 
+        "coordinates": {"x": 50.0259882, "y": 20.9640584}},
+    {
+        "name": "Warszawa",
+        "demand": 200,
+        "coordinates": {"x": 52.2319581, "y": 21.0067249},
+    },
+    {
+        "name": "Wieliczka",
+        "demand": 90,
+        "coordinates": {"x": 49.9823773, "y": 20.0602114},
+    },
+    {
+        "name": "Wrocław",
+        "demand": 40,
+        "coordinates": {"x": 51.1263106, "y": 16.9781963},
+    },
+    {
+        "name": "Zakopane",
+        "demand": 200,
+        "coordinates": {"x": 49.2969114, "y": 19.9504753},
+    },
+    {
+        "name": "Zamość",
+        "demand": 300,
+        "coordinates": {"x": 50.7170854, "y": 23.2525711},
+    }
+]
